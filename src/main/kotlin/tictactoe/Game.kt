79d43dcc50095eb01e4574bb8c7a0779dec68a26
bhallaldeva <chinmay.apte@gmail.com>
2018-12-01 17:50:44 +0100

    Adds ability to find winner for all scenarios

--- src/main/kotlin/tictactoe/Game.kt
+++ src/main/kotlin/tictactoe/Game.kt
@@ -40,6 +40,36 @@ class Game(private val players: List<Player>) {
         state[move.position.row][move.position.column] = move.player
     }
 
+    fun getWinner(): Player {
+        return winner
+    }
+
+    fun isOver(): Boolean {
+        val (rowFilled, rowWinner) = checkRows()
+        if (rowFilled) {
+            this.winner = rowWinner!!
+            return true
+        }
+
+        val (columnFilled, columnWinner) = checkColumns()
+        if (columnFilled) {
+            this.winner = columnWinner!!
+            return true
+        }
+        val (diagonalOneFilled, diagonalOneWinner) = checkDiagonalTopLeftToBottomRight()
+        if (diagonalOneFilled) {
+            this.winner = diagonalOneWinner!!
+            return true
+        }
+
+        val (diagonalTwoFilled, diagonalTwoWinner) = checkDiagonalBottomLeftToTopRight()
+        if (diagonalTwoFilled) {
+            this.winner = diagonalTwoWinner!!
+            return true
+        }
+        return allMovesMade()
+    }
+
     private fun validateMove(move: Move) {
         val position = move.position
         if ((position.row > players.size - 1) || (position.row > players.size - 1))
@@ -48,12 +78,112 @@ class Game(private val players: List<Player>) {
             throw DuplicateMoveException("Move already made")
     }
 
-    fun getWinner(): Player {
-        return winner
+    private fun checkRows(): Pair<Boolean, Player?> {
+        state.forEach { row ->
+            var tempPlayer: Player? = null
+            var foundWinner = true
+            for (column in 0 until row.size) {
+                val player = row[column]
+                if (tempPlayer == null) {
+                    if (player != null)
+                        tempPlayer = player
+                    else {
+                        foundWinner = false
+                    }
+                } else {
+                    if (tempPlayer != player) {
+                        foundWinner = false
+                    }
+                }
+            }
+            if (foundWinner)
+                return Pair(true, tempPlayer)
+        }
+        return Pair(false, null)
     }
 
-    fun isOver(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun checkColumns(): Pair<Boolean, Player?> {
+        for (column in 0 until players.size) {
+            var foundWinner = true
+            var tempPlayer: Player? = null
+            state.forEach { row ->
+                if (tempPlayer == null) {
+                    if (row[column] != null)
+                        tempPlayer = row[column]
+                    else
+                        foundWinner = false
+                } else {
+                    if (tempPlayer != row[column])
+                        foundWinner = false
+                }
+            }
+            if (foundWinner)
+                return Pair(true, tempPlayer)
+        }
+        return Pair(false, null)
+    }
+
+    private fun checkDiagonalTopLeftToBottomRight(): Pair<Boolean, Player?> {
+        var foundWinner = true
+        var tempPlayer: Player? = null
+
+        for (row in 0 until players.size) {
+            for (column in 0 until players.size) {
+                if (row == column) {
+                    if (tempPlayer == null) {
+                        if (state[row][column] != null)
+                            tempPlayer = state[row][column]
+                        else
+                            foundWinner = false
+                    } else {
+                        if (tempPlayer != state[row][column])
+                            foundWinner = false
+                    }
+                }
+            }
+        }
+
+        return if (tempPlayer != null && foundWinner)
+            Pair(true, tempPlayer)
+        else
+            Pair(false, null)
+    }
+
+    private fun checkDiagonalBottomLeftToTopRight(): Pair<Boolean, Player?> {
+        var foundWinner = true
+        var tempPlayer: Player? = null
+
+        for (row in 0 until players.size) {
+            for (column in 0 until players.size) {
+                if (row + column == players.size - 1) {
+                    if (tempPlayer == null) {
+                        if (state[row][column] != null)
+                            tempPlayer = state[row][column]
+                        else
+                            foundWinner = false
+                    } else {
+                        if (tempPlayer != state[row][column])
+                            foundWinner = false
+                    }
+                }
+            }
+
+
+        }
+        if (tempPlayer != null && foundWinner)
+            return Pair(true, tempPlayer)
+        return Pair(false, null)
+    }
+
+    private fun allMovesMade(): Boolean {
+        state.forEach { row ->
+            row.forEach { column ->
+                if (column == null)
+                    return false
+            }
+
+        }
+        return true
     }
 
     fun nextPlayer(): Player {
--- src/test/kotlin/tictactoe/GameTest.kt
+++ src/test/kotlin/tictactoe/GameTest.kt
@@ -1,6 +1,6 @@
 package tictactoe
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 import tictactoe.exception.DuplicateMoveException
 import tictactoe.exception.InvalidMoveException
@@ -10,22 +10,26 @@ class GameTest {
     private val writer = TestWriter()
 
     private val playerOne = Player("1", "X")
+    private val playerTwo = Player("2", "Y")
+    private val playerThree = Player("3", "Z")
+
     private val players = listOf(
         playerOne,
-        Player("2", "Y"),
-        Player("3", "Z")
+        playerTwo,
+        playerThree
     )
+
     @Test
     fun `given players, should print player details`() {
         val game = Game(players)
         game.printPlayerDetails(writer)
-        assertEquals("[Player ID: 1, Player Symbol: X]",writer.lines[1])
-        assertEquals("[Player ID: 2, Player Symbol: Y]",writer.lines[2])
-        assertEquals("[Player ID: 3, Player Symbol: Z]",writer.lines[3])
+        assertEquals("[Player ID: 1, Player Symbol: X]", writer.lines[1])
+        assertEquals("[Player ID: 2, Player Symbol: Y]", writer.lines[2])
+        assertEquals("[Player ID: 3, Player Symbol: Z]", writer.lines[3])
     }
 
     @Test
-    fun `given no move recorded, should print empty game state`(){
+    fun `given no move recorded, should print empty game state`() {
         val game = Game(players)
         game.initialize()
         game.printCurrentState(writer)
@@ -39,7 +43,7 @@ class GameTest {
         val game = Game(players)
         game.initialize()
         val move = Move(
-            position = Position(0,0),
+            position = Position(0, 0),
             player = playerOne
         )
         game.makeMove(move)
@@ -51,21 +55,82 @@ class GameTest {
     }
 
     @Test(expected = InvalidMoveException::class)
-    fun `given a move is made outside boundaries, it fails`(){
+    fun `given a move is made outside boundaries, it fails`() {
         val game = Game(players)
         game.initialize()
         val move = Move(
-            position = Position(4,4),
+            position = Position(4, 4),
             player = playerOne
         )
         game.makeMove(move)
     }
+
+    @Test
+    fun `given  all places are filled, isOver returns true`() {
+        val game = Game(players)
+        game.initialize()
+        game.makeMove(Move(position = Position(0, 0), player = playerOne))
+        game.makeMove(Move(position = Position(0, 1), player = playerTwo))
+        game.makeMove(Move(position = Position(0, 2), player = playerThree))
+        game.makeMove(Move(position = Position(1, 0), player = playerTwo))
+        game.makeMove(Move(position = Position(1, 1), player = playerOne))
+        game.makeMove(Move(position = Position(1, 2), player = playerThree))
+        game.makeMove(Move(position = Position(2, 0), player = playerThree))
+        game.makeMove(Move(position = Position(2, 1), player = playerOne))
+        game.makeMove(Move(position = Position(2, 2), player = playerTwo))
+        assertTrue(game.isOver())
+    }
+
+    @Test
+    fun `given a row is filled by player, isOver returns true, and player becomes winner`() {
+        val game = Game(players)
+        game.initialize()
+        game.makeMove(Move(position = Position(1, 0), player = playerOne))
+        game.makeMove(Move(position = Position(1, 1), player = playerOne))
+        game.makeMove(Move(position = Position(1, 2), player = playerOne))
+        assertTrue(game.isOver())
+        assertEquals(playerOne,game.getWinner())
+    }
+
+    @Test
+    fun `given a column is filled by player, isOver returns true, and player becomes winner`() {
+        val game = Game(players)
+        game.initialize()
+        game.makeMove(Move(position = Position(0, 1), player = playerOne))
+        game.makeMove(Move(position = Position(1, 1), player = playerOne))
+        game.makeMove(Move(position = Position(2, 1), player = playerOne))
+        assertTrue(game.isOver())
+        assertEquals(playerOne,game.getWinner())
+    }
+
+    @Test
+    fun `given a top left to bottom right diagonal is filled by player, isOver returns true, and player becomes winner`() {
+        val game = Game(players)
+        game.initialize()
+        game.makeMove(Move(position = Position(0, 0), player = playerOne))
+        game.makeMove(Move(position = Position(1, 1), player = playerOne))
+        game.makeMove(Move(position = Position(2, 2), player = playerOne))
+        assertTrue(game.isOver())
+        assertEquals(playerOne,game.getWinner())
+    }
+
+    @Test
+    fun `given a bottom left to top right diagonal is filled by player, isOver returns true, and player becomes winner`() {
+        val game = Game(players)
+        game.initialize()
+        game.makeMove(Move(position = Position(2, 0), player = playerOne))
+        game.makeMove(Move(position = Position(1, 1), player = playerOne))
+        game.makeMove(Move(position = Position(0, 2), player = playerOne))
+        assertTrue(game.isOver())
+        assertEquals(playerOne,game.getWinner())
+    }
+
     @Test(expected = DuplicateMoveException::class)
-    fun `given a duplicate move is made, it fails`(){
+    fun `given a duplicate move is made, it fails`() {
         val game = Game(players)
         game.initialize()
         val move = Move(
-            position = Position(0,0),
+            position = Position(0, 0),
             player = playerOne
         )
         game.makeMove(move)
